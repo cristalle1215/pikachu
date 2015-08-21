93fdaa70ef0aad7e0f563d6a732bcbc3ef6b052b
cristalle1215 <cristallekwok@gmail.com>
2015-08-21 08:46:28 -0700

    Pikachu

---
+++ Pikachu/pokemon.java
@@ -0,0 +1,17 @@
+
+/**
+ * pikachu
+ * 
+ * Cristalle 
+ * 8/21/15
+ */
+public class pokemon
+{
+public static void main(String[] args){
+    System.out.println("Pikachu welcomes you to the wolrd of Pokemon!");
+    System.out.println("\t(\\__/)");
+    System.out.println("\t(o^.^)");
+    System.out.println("       z(_(\")(\")");
+    }
+}
+    
